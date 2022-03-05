fed7714948ecf6962a553eaf166e0a0d728863c3
Henry <henryolson@icloud.com>
2022-03-05 10:56:45 -0600

    test

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3001;
-//const PORT = process.env.PORT || 3001;
-//const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
 
@@ -17,9 +16,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  //useFindAndModify: false
+  useFindAndModify: false
 });
 
 // routes
